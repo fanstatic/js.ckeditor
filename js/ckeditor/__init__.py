42c8ca0623648f2bc59b00cdee028f2d7d4a83cf
Jan-Jaap Driessen <devnull@localhost>
2011-06-16 22:29:03 +0200

    Add jquery adapter.

--- js/ckeditor/__init__.py
+++ js/ckeditor/__init__.py
@@ -3,3 +3,10 @@ from fanstatic import Library, Resource
 library = Library('ckeditor', 'resources')
 
 ckeditor = Resource(library, 'ckeditor_source.js', minified='ckeditor.js')
+
+try:
+    from js.jquery import jquery
+    jquery_adapter = Resource(
+        library, 'adapters/jquery.js', depends=[ckeditor, jquery])
+except ImportError:
+    pass
